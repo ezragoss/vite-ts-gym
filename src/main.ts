704043b939db689956e48abc213f7f79234e460e
ezra goss <ezragoss@gmail.com>
2025-11-08 13:47:22 -0800

    just use js-yaml directly

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import '@/style/main.css'
 
-import docs from '@/resources/posts/example_post.yaml'
+import { loadAll } from 'js-yaml'
+import docsRaw from '@/resources/posts/example_post.yaml?raw'
 
-console.log(docs.title)
-console.log(docs.body)
+const docs = loadAll(docsRaw)
+
+console.log(docs)
 
 const app = createApp(App)
 
