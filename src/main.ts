bdb2f64890c49823b3144b6e519ec98b90578223
ezra goss <ezragoss@gmail.com>
2025-11-11 21:52:42 -0800

    correct copy issue

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,6 @@ import App from './App.vue'
 import router from './router'
 import '@/style/main.css'
 
-import loadPost from './composables/yaml'
-
-const docs = await loadPost('example_post')
-
-console.log(docs)
-
 const app = createApp(App)
 
 app.use(router)
