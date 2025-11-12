04cc5c4eda9ca2013c1503524b82d2242f71292a
ezra goss <ezragoss@gmail.com>
2025-11-12 10:26:12 -0800

    Switch to fetch from import

--- src/composables/yaml.ts
+++ src/composables/yaml.ts
@@ -1,14 +1,15 @@
 import { loadAll } from 'js-yaml'
 
 async function loadYaml(path: string): Promise<Array<Record<string, any>>> {
-  const result = await import(/* @vite-ignore */ path + '?raw', { assert: { type: 'string' } })
+  console.log(path + '?raw')
+  const result = await fetch(path + '?raw')
+  const text = await result.text()
 
-  console.log(result.default)
-  return (await loadAll(result.default)) as Array<Record<string, any>>
+  return loadAll(text) as Array<Record<string, any>>
 }
 
 async function loadPost(post_name: string): Promise<Record<string, any>> {
-  const post_components = await loadYaml('/src/resources/posts/' + post_name + '.yaml')
+  const post_components = await loadYaml('../resources/posts/' + post_name + '.yaml')
   const post = post_components[0] as Record<string, any>
   post.body = post_components[1]
   return post
